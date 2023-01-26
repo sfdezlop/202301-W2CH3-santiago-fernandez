c76fed0539d1ea48368a54fb372b50f2657aced9
sfernandez <sfdezlop@gmail.com>
2023-01-26 18:08:17 +0100

    Update tests to test sonar

--- arrayfunctions.test.js
+++ arrayfunctions.test.js
@@ -11,12 +11,12 @@ describe('Testing of function arrayLength', () => {
   });
 });
 
-describe('Testing of function arrayLength', () => {
-  test('[0,0,{}] It should be 3', () => {
-    const r = arrayLength([0, 0, {}]);
-    expect(r).toBe(3);
-  });
-});
+// describe('Testing of function arrayLength', () => {
+//   test('[0,0,{}] It should be 3', () => {
+//     const r = arrayLength([0, 0, {}]);
+//     expect(r).toBe(3);
+//   });
+// });
 
 //Test basados en array de casos con Metodología Gerkin o GwT  (given when then)
 // describe('Testing of function arrayLength with a table of test cases', () => {
@@ -39,12 +39,12 @@ describe('Testing of function arrayLength', () => {
 // });
 
 //TESTS DE arrayPush
-describe('Testing de la función arrayPush', () => {
-  test('When you apply function arrayPush to array [0,0] and element 21, the new length is 3', () => {
-    const arraytest = [0, 0];
-    const elementToPush = 21;
-    arrayPush(arraytest, elementToPush);
-    const r = arrayLength(arraytest);
-    expect(r).toBe(3);
-  });
-});
+// describe('Testing de la función arrayPush', () => {
+//   test('When you apply function arrayPush to array [0,0] and element 21, the new length is 3', () => {
+//     const arraytest = [0, 0];
+//     const elementToPush = 21;
+//     arrayPush(arraytest, elementToPush);
+//     const r = arrayLength(arraytest);
+//     expect(r).toBe(3);
+//   });
+// });
